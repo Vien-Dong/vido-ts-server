a50f1938e8695759bb220c7efaace8dad356accf
supercoderl <150410325+supercoderl@users.noreply.github.com>
2024-02-03 22:08:21 +0700

    update-2-3

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
 var express = require("express");
 var cors = require('cors');
 var mongoose = require('mongoose');
-var passport = require('passport');
-var jwt = require('jsonwebtoken');
+var io = require("socket.io-client")("https://vido-ts-server.onrender.com");
+const Message = require("./models/message");
 
 require('dotenv').config();
 
 var app = express();
 
 var port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log("Listenning in port ", port);
-});
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(passport.initialize());
 
 mongoose.connect("mongodb+srv://administrator:admin123456@cluster.jh4lmtx.mongodb.net/").then(() => {
     console.log("Connected to mongodb");
@@ -24,5 +20,54 @@ mongoose.connect("mongodb+srv://administrator:admin123456@cluster.jh4lmtx.mongod
     console.log("Error connecting to mongodb: ", err);
 })
 
+let chatgroups = [];
+
+io.on("connection", (socket) => {
+    console.log(`${socket.id} user is just connected`);
+
+    // socket.on("getAllGroups", () => {
+    //     socket.emit("groupList", chatgroups);
+    // });
+
+    // socket.on("createNewGroup", (currentGroupName) => {
+    //     console.log(currentGroupName);
+    //     chatgroups.unshift({
+    //         id: chatgroups.length + 1,
+    //         currentGroupName,
+    //         messages: [],
+    //     });
+    //     socket.emit("groupList", chatgroups);
+    // });
+
+    socket.on("findGroup", async (id) => {
+        const filteredGroup = await Message.find({ "roomId": id });
+        chatgroups = filteredGroup;
+        socket.emit("foundGroup", filteredGroup);
+    });
+
+    socket.on("newChatMessage", (data) => {
+        const { roomId, sender, messageType, messageText } = data;
+        const obj = {
+            roomId,
+            sender,
+            messageType,
+            message: messageText,
+            timeStamp: new Date(),
+        }
+
+        const messageMongo = new Message(obj);
+        messageMongo.save();
+
+        socket.emit("groupMessage", obj);
+        chatgroups.push(obj);
+        // socket.emit("groupList", chatgroups);
+        socket.emit("foundGroup", chatgroups);
+    });
+})
+
 app.use('/api/notification', require('./routes/api/notification'));
 app.use('/api/chat', require('./routes/api/chat'));
+
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
