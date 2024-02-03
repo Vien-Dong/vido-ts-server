f6bdbe99b947d5e0712ca1f64f75aaf365d8058c
supercoderl <150410325+supercoderl@users.noreply.github.com>
2024-02-03 15:21:09 +0700

    update-2-3

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require("express");
 var cors = require('cors');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var jwt = require('jsonwebtoken');
 
 require('dotenv').config();
 
@@ -13,5 +16,13 @@ app.listen(port, () => {
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(passport.initialize());
+
+mongoose.connect("mongodb+srv://administrator:admin123456@cluster.jh4lmtx.mongodb.net/").then(() => {
+    console.log("Connected to mongodb");
+}).catch((err) => {
+    console.log("Error connecting to mongodb: ", err);
+})
 
 app.use('/api/notification', require('./routes/api/notification'));
+app.use('/api/chat', require('./routes/api/chat'));
