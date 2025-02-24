5a24f05bd05550ccb8a2c751f7d19c13bec63024
tdat <datnguyen4804@gmail.com>
2025-02-24 08:53:00 +0700

    landing-page

--- pages/landing-page/crm.js
+++ pages/landing-page/crm.js
@@ -1,26 +1,25 @@
 $(document).ready(function () {
-
     let Training_industry = ["Kế toán", "QT kinh doanh", "QT văn phòng (Thư ký y khoa)",
         "Logistics", "QT Marketing", "Quan hệ công chúng", "Tài chính - ngân hàng", "Ô tô", "Cơ khí", "Xây dựng", "Điện - Điện Tử - Điện lạnh",
         "Thiết kế đồ họa", "Lập trình ứng dụng", "Truyền thông đa phương tiện", "Truyền thông mạng máy tính", "Dược Sĩ", "Y sĩ đa khoa", "Điều dưỡng", "Chăm sóc sắc đẹp", "Hộ sinh", "Xét nghiệm",
         "Biên phiên dịch", "Tiếng anh thương mai", "QT khách sạn", "Du lịch & lữ hành", "NV nhà hàng - khách sạn", "Sư phạm mầm non", "Giáo dục mầm non", "Tiếng anh sư phạm"
-      ];
+    ];
 
-      let industrySelect = $('select[name="Training_industry"]');
+    let industrySelect = $('select[name="Training_industry"]');
     Training_industry.forEach((industry) => {
         industrySelect.append(`<option value="${industry}">${industry}</option>`);
-    })
+    });
 
-    $(document).on("submit", ".ladi-form", function (event) {
+    // Bắt sự kiện khi click vào button mới
+    $(document).on("click", ".btn-submit-custom", function (event) {
         event.preventDefault();
 
         var inputNameValue = $('input[name="name"]').val();
-        var selectedIndustry = $('select[name="Training_industry"]').val(); // Lấy ngành học đã chọn
+        var selectedIndustry = $('select[name="Training_industry"]').val();
         var inputPhoneValue = $('input[name="phone"]').val();
         var inputStreetValue = $('input[name="street"]').val();
 
-        // Kiểm tra dữ liệu nhập vào
-        if (!inputNameValue || !inputPhoneValue || !inputStreetValue     || !selectedIndustry) {
+        if (!inputNameValue || !inputPhoneValue || !inputStreetValue || !selectedIndustry) {
             alert("Vui lòng điền đầy đủ thông tin!");
             return;
         } else if (inputNameValue.length < 3) {
@@ -31,7 +30,6 @@ $(document).ready(function () {
             return;
         }
 
-        // Tách họ và tên
         var names = inputNameValue.split(" ");
         var firstName = names[names.length - 1];
         var lastName = names.slice(0, -1).join(" ");
@@ -54,15 +52,14 @@ $(document).ready(function () {
             address: "",
             consulting_staff: "",
             assigned_user_id: "3",
-          };
+        };
 
-          console.log (postData);
-        // Gửi dữ liệu bằng Axios
+        console.log(postData);
         axios.post("/api/crm/create-cptarget", postData)
             .then((result) => {
                 if (result.data && result.data.success) {
                     alert("Gửi thông tin thành công!");
-                    $(".ladi-form")[0].reset(); // Reset form sau khi gửi thành công
+                    $(".ladi-form")[0].reset();
                 }
             })
             .catch((error) => {
