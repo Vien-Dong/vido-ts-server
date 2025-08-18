122dd4017ed8d84e0c6796617b6d78ae2975a90f
tdat <datnguyen4804@gmail.com>
2025-08-18 11:05:27 +0700

    18/8

--- services/crm-service.js
+++ services/crm-service.js
@@ -3,7 +3,7 @@ const Participant = require("../models/participant");
 const Device = require("../models/device");
 const Redis = require("ioredis");
 
-const redis = new Redis("rediss://default:AeDjAAIjcDFmZTlhMTFhMjIwNzg0NDY2YmRmNmUyMjJhYzRjZGIxZnAxMA@pleasant-panther-57571.upstash.io:6379");
+const redis = new Redis("rediss://default:Ac4BAAIncDE4OWU3NjhiOGU4YTY0ZDYwYTMxZDIxMDMzNTQyZGE2ZHAxNTI3Mzc@elegant-sawfly-52737.upstash.io:6379");
 
 // Cache trên server
 let cachedToken = null;
